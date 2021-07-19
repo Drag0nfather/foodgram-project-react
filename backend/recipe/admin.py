0e4de138d92c846e7c9a23d8cd5722688b2c375d
Andrew <Andrey18651@gmail.com>
2021-07-19 22:09:11 +0300

    register models Tag, Ingredients and Recipe on admin panel

--- backend/recipe/admin.py
+++ backend/recipe/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Tag, Ingredient, Recipe
+
+
+class AuthorAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(Tag)
+admin.site.register(Ingredient)
+admin.site.register(Recipe)
