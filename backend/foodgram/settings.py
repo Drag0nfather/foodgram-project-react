f8c1feaed01952f40c73b22e5e4aba814446ca81
Andrew <Andrey18651@gmail.com>
2021-07-22 17:57:42 +0300

    add installed apps in settings, add token authentication

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -42,6 +42,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'rest_framework',
+
+    'recipe',
+    'user',
 ]
 
 MIDDLEWARE = [
@@ -125,3 +130,16 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+# DRF Settings
+
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ],
+
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+    ]
+}
