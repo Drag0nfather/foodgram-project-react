97f45d717fc5acb97ba465350081675bbe283b62
Andrew <Andrey18651@gmail.com>
2021-07-23 19:01:57 +0300

    add users url

--- backend/user/urls.py
+++ backend/user/urls.py
@@ -1,9 +1,17 @@
 from django.conf.urls import url
+from django.urls import path, include
 from rest_framework.authtoken import views
+from rest_framework.routers import DefaultRouter
 
 from user.utils.logout import Logout
+from user.views import UserViewSet
+
+
+router = DefaultRouter()
+router.register(r'users', UserViewSet)
 
 urlpatterns = [
-    url('auth/token/login/', views.obtain_auth_token),
-    url(r'auth/token/logout/', Logout.as_view()),
+    path('auth/token/login/', views.obtain_auth_token),
+    path('auth/token/logout/', Logout.as_view()),
+    path('', include(router.urls))
 ]
