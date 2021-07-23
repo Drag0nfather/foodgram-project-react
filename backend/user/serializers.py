e32ae96692806374774117833d35bae9a9c7a318
Andrew <Andrey18651@gmail.com>
2021-07-23 20:41:49 +0300

    add ChangePasswordSerializer

--- backend/user/serializers.py
+++ backend/user/serializers.py
@@ -8,3 +8,12 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         fields = ('id', 'email', 'username', 'first_name', 'last_name', 'password')
         model = User
+
+
+class ChangePasswordSerializer(serializers.Serializer):
+    """
+    Serializer for password change endpoint.
+    """
+    model = User
+    new_password = serializers.CharField(required=True)
+    current_password = serializers.CharField(required=True)
