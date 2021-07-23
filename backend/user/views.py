1bec1f5e17c83f05e56be078fa30391a43736ddf
Andrew <Andrey18651@gmail.com>
2021-07-23 20:42:24 +0300

    add action set_password in UserViewSet

--- backend/user/views.py
+++ backend/user/views.py
@@ -1,12 +1,12 @@
 from http import HTTPStatus
 
-from django.shortcuts import render
 from rest_framework import viewsets, status
 from rest_framework.decorators import action
 from rest_framework.response import Response
+from rest_framework.views import APIView
 
 from user.models import User
-from user.serializers import UserSerializer
+from user.serializers import UserSerializer, ChangePasswordSerializer
 
 
 class UserViewSet(viewsets.ModelViewSet):
@@ -25,3 +25,19 @@ class UserViewSet(viewsets.ModelViewSet):
             serializer = UserSerializer(user)
             return Response(serializer.data)
         return Response(status=HTTPStatus.UNAUTHORIZED)
+
+    @action(methods=['post'],
+            serializer_class=ChangePasswordSerializer,
+            detail=False)
+    def set_password(self, request):
+        serializer = ChangePasswordSerializer(data=request.data)
+        if serializer.is_valid():
+            if not request.user.check_password(
+                    serializer.data.get('current_password')):
+                return Response({'current_password': ['Wrong password.']},
+                                status=status.HTTP_400_BAD_REQUEST)
+            request.user.set_password(serializer.data.get('new_password'))
+            request.user.save()
+            return Response({'status': 'password set'},
+                            status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
