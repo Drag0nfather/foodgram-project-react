754f117d023f9d6e019a123c7a8932ca9e587b59
Andrew <Andrey18651@gmail.com>
2021-07-24 14:20:57 +0300

    add tags/ url

---
+++ backend/recipe/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+
+from recipe.views import TagViewSet
+
+router = DefaultRouter()
+
+router.register('tags', TagViewSet)
+
+urlpatterns = [
+    path('', include(router.urls)),
+]
