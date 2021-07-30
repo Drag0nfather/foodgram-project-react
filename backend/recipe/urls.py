aa1ef320e94d3e2e94ed98eaf3b2e023e1bc2ebd
Andrew <Andrey18651@gmail.com>
2021-07-30 16:24:27 +0300

    add ingredients route

--- backend/recipe/urls.py
+++ backend/recipe/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 
-from recipe.views import TagViewSet, RecipeViewSet
+from recipe.views import TagViewSet, RecipeViewSet, IngredientViewSet
 
 router = DefaultRouter()
 
 router.register(r'tags', TagViewSet)
 router.register(r'recipes', RecipeViewSet)
+router.register(r'ingredients', IngredientViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
