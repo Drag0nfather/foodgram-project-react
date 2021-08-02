9344fdc1936deb1ea2e1b7aeb4f656d61d397b4d
Andrew <Andrey18651@gmail.com>
2021-08-02 19:50:02 +0300

    rename field colour to color in Tag model

--- backend/recipe/models.py
+++ backend/recipe/models.py
@@ -7,7 +7,7 @@ User = get_user_model()
 
 class Tag(models.Model):
     name = models.CharField(max_length=50, blank=False, unique=True,)
-    colour = models.CharField(max_length=50, blank=False, unique=True)
+    color = models.CharField(max_length=50, blank=False, unique=True)
     slug = models.SlugField(max_length=50, blank=False, unique=True)
 
     def __str__(self):
