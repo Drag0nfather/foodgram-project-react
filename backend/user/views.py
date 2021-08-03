12845ca39ec59d9077d7009765060a2fb54c115d
Andrew <Andrey18651@gmail.com>
2021-08-03 21:15:01 +0300

    update action "me", add action subscriptions and subscribe

--- backend/user/views.py
+++ backend/user/views.py
@@ -2,10 +2,12 @@ from http import HTTPStatus
 
 from rest_framework import viewsets, status
 from rest_framework.decorators import action
+from rest_framework.generics import get_object_or_404
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 
-from user.models import User
-from user.serializers import UserSerializer, ChangePasswordSerializer
+from user.models import User, Follow
+from user.serializers import UserSerializer, ChangePasswordSerializer, SubscriptionSerializer
 
 
 class UserViewSet(viewsets.ModelViewSet):
@@ -15,15 +17,11 @@ class UserViewSet(viewsets.ModelViewSet):
     serializer_class = UserSerializer
     queryset = User.objects.all()
 
-    @action(methods=['get'], detail=False)
+    @action(detail=False, methods=['get'],
+            permission_classes=[IsAuthenticated])
     def me(self, request):
-        if self.request.user is None:
-            return Response(status=HTTPStatus.INTERNAL_SERVER_ERROR)
-        if self.request.user.is_authenticated:
-            user = User.objects.get(id=request.user.id)
-            serializer = UserSerializer(user)
-            return Response(serializer.data)
-        return Response(status=HTTPStatus.UNAUTHORIZED)
+        serializer = self.get_serializer(self.request.user)
+        return Response(serializer.data, status=status.HTTP_200_OK)
 
     @action(methods=['post'],
             serializer_class=ChangePasswordSerializer,
@@ -40,3 +38,42 @@ class UserViewSet(viewsets.ModelViewSet):
             return Response({'status': 'password set'},
                             status=status.HTTP_200_OK)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    @action(detail=False, methods=['get'],
+            permission_classes=[IsAuthenticated])
+    def subscriptions(self, request):
+        queryset = User.objects.filter(
+            following__user=request.user).order_by('id')
+        page = self.paginate_queryset(queryset)
+        if page is not None:
+            serializer = SubscriptionSerializer(page, many=True,
+                                                context={'request': request})
+            return self.get_paginated_response(serializer.data)
+        serializer = SubscriptionSerializer(page, many=True,
+                                            context={'request': request})
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    @action(detail=True,
+            methods=['get', 'delete'],
+            permission_classes=[IsAuthenticated])
+    def subscribe(self, request, pk):
+        author = get_object_or_404(User, pk=pk)
+        user = request.user
+        followed = user.follower.filter(author=author).exists()
+        if request.method == 'GET':
+            if author != user and not followed:
+                Follow.objects.create(user=user, author=author)
+                serializer = UserSerializer(author,
+                                            context={'request': request})
+                return Response(data=serializer.data,
+                                status=status.HTTP_201_CREATED)
+            data = {'errors': 'Вы или уже подписаны на этого автора, или '
+                              'пытаетесь подписаться на себя, что невозможно'}
+            return Response(data=data, status=status.HTTP_403_FORBIDDEN)
+        if not user.follower.filter(author=author).exists():
+            data = {'errors': 'Вы не подписаны на данного автора '
+                              'напоминание: на себя подписаться невозможно)'}
+            return Response(data=data, status=status.HTTP_400_BAD_REQUEST)
+        Follow.objects.filter(user=user, author=author).delete()
+        data = {'deleted': 'success'}
+        return Response(data=data, status=status.HTTP_204_NO_CONTENT)
