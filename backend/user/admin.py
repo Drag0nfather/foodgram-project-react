c0f5b106d0f8e4f0113f66b08e886048657067f7
Andrew <Andrey18651@gmail.com>
2021-08-06 18:27:29 +0300

    update user.admin: add filters by first_name and email

--- backend/user/admin.py
+++ backend/user/admin.py
@@ -1,12 +1,15 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-from user.models import Follow, ShoppingCart, FavouriteRecipe
+from user.models import Follow, ShoppingCart, FavouriteRecipe, User
 
 
-class AuthorAdmin(admin.ModelAdmin):
-    pass
+class UserAdmin(UserAdmin):
+    list_filter = ("first_name", "email")
 
 
 admin.site.register(Follow)
 admin.site.register(ShoppingCart)
 admin.site.register(FavouriteRecipe)
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
