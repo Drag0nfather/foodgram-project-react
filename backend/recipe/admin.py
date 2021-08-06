c89a731fa59e69ae26a679787554361cdeb97642
Andrew <Andrey18651@gmail.com>
2021-08-06 15:06:37 +0300

    add RecipeAdmin model

--- backend/recipe/admin.py
+++ backend/recipe/admin.py
@@ -1,13 +1,25 @@
 from django.contrib import admin
+from django.shortcuts import get_object_or_404
 
 from .models import Tag, Recipe, Ingredient, IngredientInRecipe
 
 
+class RecipeIngredientInLine(admin.TabularInline):
+    model = IngredientInRecipe
+
+
+@admin.register(Recipe)
 class RecipeAdmin(admin.ModelAdmin):
-    pass
+    list_display = ('name', 'author', 'is_favorited')
+    search_fields = ('user', 'author')
+    list_filter = ('author', 'name', 'tags')
+    inlines = [RecipeIngredientInLine]
+
+    def is_favorited(self, request):
+        recipe = get_object_or_404(Recipe, id=request.id)
+        return recipe.is_favorited.count()
 
 
 admin.site.register(Tag)
 admin.site.register(Ingredient)
-admin.site.register(Recipe)
 admin.site.register(IngredientInRecipe)
