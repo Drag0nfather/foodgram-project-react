4b0d83a882eaadd0fae32217ee780ea671412ded
Andrew <Andrey18651@gmail.com>
2021-08-10 17:40:10 +0300

    change users urls

--- backend/user/urls.py
+++ backend/user/urls.py
@@ -1,16 +1,17 @@
-from django.urls import path, include
+from django.urls import path, re_path
 from rest_framework.authtoken import views
-from rest_framework.routers import DefaultRouter
 
 from user.utils.logout import Logout
 from user.views import UserViewSet
 
 
-router = DefaultRouter()
-router.register(r'users', UserViewSet)
-
 urlpatterns = [
     path('auth/token/login/', views.obtain_auth_token),
     path('auth/token/logout/', Logout.as_view()),
-    path('', include(router.urls))
+    re_path(r'^auth/users/$', UserViewSet.as_view({'post': 'create_user'})),
+    re_path(r'^users$', UserViewSet.as_view({'get': 'list'})),
+    re_path(r'^users/(?P<pk>[0-9]+)/$', UserViewSet.as_view({'get': 'retrieve'})),
+    re_path(r'^users/set_password/$', UserViewSet.as_view({'post': 'set_password'})),
+    re_path(r'^users/subscriptions/$', UserViewSet.as_view({'get': 'subscriptions'})),
+    re_path(r'^users/(?P<pk>[0-9]+)/subscribe/$', UserViewSet.as_view({'get': 'subscribe', 'delete': 'subscribe'})),
 ]
