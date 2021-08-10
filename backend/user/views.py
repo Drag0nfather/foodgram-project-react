c1bc60e2f4db50cccb67c757e79ff8820e05916c
Andrew <Andrey18651@gmail.com>
2021-08-10 17:39:56 +0300

    change UserViewSet

--- backend/user/views.py
+++ backend/user/views.py
@@ -1,27 +1,28 @@
-from http import HTTPStatus
-
 from rest_framework import viewsets, status
 from rest_framework.decorators import action
 from rest_framework.generics import get_object_or_404
 from rest_framework.permissions import IsAuthenticated, AllowAny, IsAdminUser
 from rest_framework.response import Response
 
+from user.handler import UserHandler
 from user.models import User, Follow
-from user.serializers import UserSerializer, ChangePasswordSerializer, SubscriptionSerializer
+from user.serializers import UserSerializer, ChangePasswordSerializer, SubscriptionSerializer, UserInputSerializer, \
+    UserOutputSerializer
 
 
-class UserViewSet(viewsets.ModelViewSet):
+class UserViewSet(viewsets.ViewSet):
     """
     List all users or create a new user
     """
     serializer_class = UserSerializer
     queryset = User.objects.all()
     permission_classes = [AllowAny]
+    user_handler = UserHandler()
 
     @action(detail=False, methods=['get'],
             permission_classes=[IsAuthenticated])
     def me(self, request):
-        serializer = self.get_serializer(self.request.user)
+        serializer = self.serializer_class(self.request.user)
         return Response(serializer.data, status=status.HTTP_200_OK)
 
     @action(methods=['post'],
@@ -45,13 +46,12 @@ class UserViewSet(viewsets.ModelViewSet):
     def subscriptions(self, request):
         queryset = User.objects.filter(
             following__user=request.user).order_by('id')
-        page = self.paginate_queryset(queryset)
-        if page is not None:
-            serializer = SubscriptionSerializer(page, many=True,
-                                                context={'request': request})
-            return self.get_paginated_response(serializer.data)
-        serializer = SubscriptionSerializer(page, many=True,
-                                            context={'request': request})
+        if queryset is not None:
+            serializer = SubscriptionSerializer(
+                queryset, many=True, context={'request': request})
+            return Response(serializer.data)
+        serializer = SubscriptionSerializer(
+            queryset, many=True, context={'request': request})
         return Response(serializer.data, status=status.HTTP_200_OK)
 
     @action(detail=True,
@@ -78,3 +78,20 @@ class UserViewSet(viewsets.ModelViewSet):
         Follow.objects.filter(user=user, author=author).delete()
         data = {'deleted': 'success'}
         return Response(data=data, status=status.HTTP_204_NO_CONTENT)
+
+    def create_user(self, request):
+        data = request.data
+        serializer = UserInputSerializer(data)
+        user = self.user_handler.create_user(serializer.data)
+        serialized_user = UserOutputSerializer(user)
+        return Response(data=serialized_user.data, status=status.HTTP_201_CREATED)
+
+    def list(self, request):
+        queryset = self.user_handler.list_user()
+        serializer = UserSerializer(queryset, many=True)
+        return Response(data=serializer.data, status=status.HTTP_200_OK)
+
+    def retrieve(self, request, pk):
+        queryset = self.user_handler.get_user(pk)
+        serializer = UserSerializer(queryset)
+        return Response(data=serializer.data, status=status.HTTP_200_OK)
