68eeb959e0312d9ea7ab63e44b33b86a06375ff0
Andrew <Andrey18651@gmail.com>
2021-08-11 12:53:28 +0300

    update urls

--- backend/user/urls.py
+++ backend/user/urls.py
@@ -9,7 +9,7 @@ urlpatterns = [
     path('auth/token/login/', views.obtain_auth_token),
     path('auth/token/logout/', Logout.as_view()),
     re_path(r'^auth/users/$', UserViewSet.as_view({'post': 'create_user'})),
-    re_path(r'^users$', UserViewSet.as_view({'get': 'list'})),
+    re_path(r'^users/$', UserViewSet.as_view({'get': 'list'})),
     re_path(r'^users/(?P<pk>[0-9]+)/$', UserViewSet.as_view({'get': 'retrieve'})),
     re_path(r'^users/set_password/$', UserViewSet.as_view({'post': 'set_password'})),
     re_path(r'^users/subscriptions/$', UserViewSet.as_view({'get': 'subscriptions'})),
